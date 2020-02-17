0c51035ea3b426c7dbbe7c4069156f9413413b69
Ryoto313 <ryouto313@me.com>
2020-02-17 19:05:37 +0900

    ~24~24~24~24~24~24~24~24~24~24~24~24~24~24~24~24~24~24~24~24~24~24~24~24

--- knock20.py
+++ knock20.py
@@ -2,9 +2,10 @@ import gzip
 import json
 fname = 'jawiki-country.json.gz'
 
-with gzip.open(fname, 'rt',encoding="utf-8") as data_file:
+with gzip.open(fname, 'rt',encoding="utf-8") as data_file, open("UK.txt",mode="w",encoding="utf-8") as UK_file:
     for line in data_file:
         data_json = json.loads(line)
         if data_json['title'] == 'イギリス':
             print(data_json['text'])
+            UK_file.write(str(data_json["text"]))
             break
---
+++ knock23.py
@@ -0,0 +1,18 @@
+import gzip
+import json
+import re
+fname = 'jawiki-country.json.gz'
+def get_UK():
+    with gzip.open(fname, 'rt',encoding="utf-8") as data_file:
+        for line in data_file:
+            data_json = json.loads(line)
+            if data_json['title'] == 'イギリス':
+                return(data_json['text'])
+
+    raise ValueError("イギリスの記事が見つからない")
+
+pattern = re.compile(r'''^(=+) ?([^= ]*) ?=+$''',re.MULTILINE)
+result = pattern.findall(get_UK())
+for line in result:
+    level = len(line[0]) - 1
+    print("\t"*(level-1)+line[1]+"("+str(level)+")")
